061be74ccab40dee1f8f670378237297be4ed813
mouhongduo <51589624+mouhongduo@users.noreply.github.com>
2021-06-03 14:29:55 +0800

    初始化uni-app(含有uni-ui)

---
+++ uni-app trunk/pages/index/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<uni-badge class="uni-badge-left-margin" :text="value" absolute="rightTop" size="small" type="primary">
+			<view class="box"><text class="box-text">右上</text></view>
+		</uni-badge>
+		<uni-search-bar>搜索框</uni-search-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
